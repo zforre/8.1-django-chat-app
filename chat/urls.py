ab3aa52599d9410c40123341ac109f81bdc01c82
Zach Forrest <zachforre@gmail.com>
2020-03-03 08:35:32 -0500

    updates templates

--- chat/urls.py
+++ chat/urls.py
@@ -6,8 +6,8 @@ app_name= 'chat'
 
 urlpatterns = [
    path('<int:pk>/comments/add/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('add/', views.ChatCreateView.as_view(), name='chat_create'),
-    path('<int:pk>/', views.ChatDetailView.as_view(), name='chat_detail'),
+   path('add/', views.ChatCreateView.as_view(), name='chat_create'),
+   path('<int:pk>/', views.ChatDetailView.as_view(), name='chat_detail'),
    path('', views.ChatListView.as_view(), name='chat_list') 
 ]
 
